8227bc8ecd295706a135d0134db85fc5b92cd48f
CodeEnder <alricost@gmail.com>
2021-01-15 14:07:55 -0500

    Fixed Transparency issue
    This is a really stupid solution

--- Knives, Guns, and Ghosts/Assets/Scripts/Body.gd
+++ Knives, Guns, and Ghosts/Assets/Scripts/Body.gd
@@ -1,27 +1,14 @@
 extends Node2D
 
-onready var ankles = $Ankles
-onready var hair = $Hair
-onready var hands = $Hands
-onready var head = $Head
-onready var hoodie = $Hoodie
-onready var pants = $Pants
-onready var shoes = $Shoes
+var children
+
+func _ready():
+	children = get_children()
 
 func anim_walking():
-	ankles.play("Walking")
-	hair.play("Walking")
-	hands.play("Walking")
-	head.play("Walking")
-	hoodie.play("Walking")
-	pants.play("Walking")
-	shoes.play("Walking")
+	for i in children:
+		i.play("Walking")
 
 func anim_idle():
-	ankles.play("Idle")
-	hair.play("Idle")
-	hands.play("Idle")
-	head.play("Idle")
-	hoodie.play("Idle")
-	pants.play("Idle")
-	shoes.play("Idle")
+	for i in children:
+		i.play("Idle")
--- Knives, Guns, and Ghosts/Assets/Scripts/Lights.gd
+++ Knives, Guns, and Ghosts/Assets/Scripts/Lights.gd
@@ -4,13 +4,17 @@ const light = preload("res://Assets/Scenes/Instances/Light.tscn")
 const peripheral = preload("res://Assets/Scenes/Instances/Peripheral.tscn")
 const flashlight = preload("res://Assets/Scenes/Instances/Flashlight.tscn")
 
+onready var timer = $LightTimer
+
 var lightInstance
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_light_to_scene()
 	#add_flashlight_to_scene()
-	add_peripheral_to_scene()
+	timer.connect("timeout", self, "add_peripheral_to_scene")
+	timer.start()
+	#add_peripheral_to_scene()
 
 
 func add_light_to_scene():
